4ec602719b200bdef7c217234a781b6d6163847c
Victor Silva <victorsilva698@usp.br>
2021-01-19 22:14:11 -0300

    Codigos do curso adicionado

---
+++ operadores.py
@@ -0,0 +1,27 @@
+#   Todas as operaçoes funcionam com inteiro e float
+#   mas nem todas funcionam com string
+
+primeiroFator = 2
+segundoFator = 3
+
+resultado = primeiroFator + segundoFator
+print(resultado) # resultado = 5
+
+resultado = primeiroFator - segundoFator
+print(resultado) # resultado = -1
+
+
+resultado = primeiroFator * segundoFator
+print(resultado) # resultado = 6
+
+resultado = primeiroFator/ segundoFator
+print(resultado) # resultado = 0.666666666
+
+resultado = primeiroFator// segundoFator
+print(resultado) # resultado = 0
+
+resultado = primeiroFator % segundoFator
+print(resultado) # resultado = 2
+
+resultado = primeiroFator ** segundoFator
+print(resultado) # resultado = 8
---
+++ variaveis.py
@@ -0,0 +1,14 @@
+
+#string
+texto = 'So um texto'
+outroTexto = "Outro texto"
+
+#inteiro
+numero = 1
+
+#float
+decimal = 1.2
+
+#boolean
+eVerdadeiro = True
+eFalso = False
